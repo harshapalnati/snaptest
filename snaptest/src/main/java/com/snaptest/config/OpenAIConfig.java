bc234d80ee0943f2a35b11f65b22661bac7f9b0b
harshapalnati <harshapalnati@gmail.com>
2025-02-18 12:43:31 -0500

    set up openAi config

--- snaptest/src/main/java/com/snaptest/config/OpenAIConfig.java
+++ snaptest/src/main/java/com/snaptest/config/OpenAIConfig.java
@@ -1,4 +1,17 @@
 package com.snaptest.config;
 
-public class OpenAIConfig {
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAIConfig
+{
+    private final Dotenv dotenv = Dotenv.load();
+
+    @Bean
+    public String openAPiKey()
+    {
+        return dotenv.get("OPENAI_API_KEY");
+    }
 }
