92eb79b5da100c7d54ef99b562265d9d54370feb
harshapalnati <harshapalnati@gmail.com>
2025-02-18 12:02:07 -0500

    Init spring boot project

---
+++ snaptest/src/main/java/com/snaptest/SnaptestApplication.java
@@ -0,0 +1,13 @@
+package com.snaptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaptestApplication.class, args);
+	}
+
+}
